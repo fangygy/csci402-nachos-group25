af1e83dc838ec2169f1da91094d7b7cb3ed0fe92
yinlertc@gmail.com <yinlertc@gmail.com@6e91ac2e-7172-fd64-087b-c4397769e289>
2011-02-18 06:59:48 +0000

    Yin: Tentative test cases for yield + its Makefile

---
+++ trunk/Project 2/test/yieldTest.c
@@ -0,0 +1,15 @@
+/* yieldTest.c
+ *	
+ * Test program to test whether the yield functionality works.
+ *	
+ *	
+ *
+ */
+
+#include "syscall.h"
+
+int main()
+{
+    Yield();
+    /* not reached */
+}
