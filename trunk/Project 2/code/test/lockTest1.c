82117a39d237ac8511b7972ee09033ea6caf2e32
yinlertc@gmail.com <yinlertc@gmail.com@6e91ac2e-7172-fd64-087b-c4397769e289>
2011-02-21 02:34:53 +0000

    Yin: Making a lock test

---
+++ trunk/Project 2/code/test/lockTest1.c
@@ -0,0 +1,19 @@
+/* lockTest1.c
+ *	
+ * Test program to test functionality of locks
+ * Create, Acquire, Release and Destroy
+ *	
+ *	
+ *
+ */
+
+#include "syscall.h"
+
+
+int main()
+{
+	int lock1, lock 2;
+	lock1 = CreateLock();
+	lock2 = CreateLock();
+	/* not reached */
+}
