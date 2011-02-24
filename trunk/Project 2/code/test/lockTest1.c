994c329861d1c4c436186000d1afca9400e23994
yinlertc@gmail.com <yinlertc@gmail.com@6e91ac2e-7172-fd64-087b-c4397769e289>
2011-02-24 08:08:21 +0000

    Yin: Revised lock test. Use as model for future tests.

--- trunk/Project 2/code/test/lockTest1.c
+++ trunk/Project 2/code/test/lockTest1.c
@@ -14,12 +14,17 @@ int main()
 {
 	int lockIndex;
 	Write("Starting lock test\n", 64, ConsoleOutput);
+
+	Write("Attempting to destroy a lock...\n", sizeof("Attempting to destroy lock...\n"), ConsoleOutput);
 	DestroyLock(0);
+	DestroyLock(-1);
 	lockIndex = CreateLock("Testlock\n", 64);
 	Release(lockIndex);
+	Release(-1);
 
 	Acquire(lockIndex);
 	Acquire(lockIndex);
+	Acquire(-1);
 	Release(lockIndex);
 	DestroyLock(lockIndex);
 
