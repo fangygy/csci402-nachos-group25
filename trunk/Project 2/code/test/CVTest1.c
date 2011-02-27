d98c7476967e9181bb45dba6a4b3a076257a3552
yinlertc@gmail.com <yinlertc@gmail.com@6e91ac2e-7172-fd64-087b-c4397769e289>
2011-02-27 01:50:39 +0000

    Yin: Made a CV test

--- trunk/Project 2/code/test/CVTest1.c
+++ trunk/Project 2/code/test/CVTest1.c
@@ -9,8 +9,37 @@
 
 #include "syscall.h"
 
+int lock;
+int condition;
+
+void testFunc(){
+	Write("Acquiring lock and waiting on it. \n", sizeof("Acquiring lock and waiting on it. \n"), ConsoleOutput);
+	Acquire(lock);
+	Wait(condition, lock);
+	Write("Woke up from wait on a signal. Releasing lock.\n", sizeof("Woke up from wait on a signal. Releasing lock.\n"), ConsoleOutput);
+	Release(lock);
+	
+	Write("Waiting on a bad lock. \n", sizeof("Waiting on a bad lock. \n"), ConsoleOutput);
+	Wait(100, 100);
+	Wait(-1, -1);
+	Exit(0);
+}
 
 int main()
 {
+	Write("Starting condition variable test\n", sizeof("Starting condition variable test\n"), ConsoleOutput);
+	
+	lock = CreateLock("TestLock", 16);
+	condition = CreateCondition("TestCondition", 23); 
+
+	Fork(testFunc);
+	Yield();
+	Acquire(lock);	/* make sure the testFunc acquires the lock first */
+
+	Write("Signalling condition on the lock\n", sizeof("Signalling condition on the lock\n"), ConsoleOutput);
+	Signal(condition, lock);
+	Yield();
+	Write("Acquiring lock. This should succeed.\n", sizeof("Acquiring lock. This should succeed.\n"), ConsoleOutput);
+	Acquire(lock);
 	/* not reached */
 }
--- trunk/Project 2/code/test/lockTest1.c
+++ trunk/Project 2/code/test/lockTest1.c
@@ -15,21 +15,27 @@ int main()
 	int lockIndex;
 	Write("Starting lock test 1\n", sizeof("Starting lock test 1\n"), ConsoleOutput);
 
-	Write("Destroying nonexistant lock...\n", sizeof("Destroying nonexistant lock...\n"), ConsoleOutput);
+	Write("Destroying nonexistant locks...\n", sizeof("Destroying nonexistant locks...\n"), ConsoleOutput);
 	DestroyLock(0);
 	DestroyLock(-1);
 
 	Write("Creating a lock...\n", sizeof("Creating a lock...\n"), ConsoleOutput);
 	lockIndex = CreateLock("Testlock", 16);
 
-	Write("Releasing locks...\n", sizeof("Releasing locks...\n"), ConsoleOutput);	
-	Release(lockIndex);
+	Write("Releasing nonexistant locks...\n", sizeof("Releasing nonexistant locks...\n"), ConsoleOutput);	
 	Release(-1);
+	Release(100);
 
-	Write("Acquiring locks...\n", sizeof("Acquiring locks...\n"), ConsoleOutput);
-	Acquire(lockIndex);
-	Acquire(lockIndex);
+	Write("Releasing non-owned lock...\n", sizeof("Releasing non-owned lock...\n"), ConsoleOutput);	
+	Release(lockIndex);
+
+	Write("Acquiring nonexistant locks...\n", sizeof("Acquiring nonexistant locks...\n"), ConsoleOutput);
 	Acquire(-1);
+	Acquire(100);
+
+	Write("Acquiring lock twice...\n", sizeof("Acquiring locks twice...\n"), ConsoleOutput);
+	Acquire(lockIndex);
+	Acquire(lockIndex);
 	Release(lockIndex);
 
 	Write("Destroying lock already released...\n", sizeof("Destroying lock already released...\n"), ConsoleOutput);
